3e6fe29b0b79a5993dfe197386ffab7bd2aba3fb
汪宏斌 <35726025+weilaihaojiubujian@users.noreply.github.com>
2020-02-29 13:57:17 +0800

    登陆

---
+++ src/api.js
@@ -0,0 +1,12 @@
+const root = 'http://localhost:8081/';
+const authenticationUrl = root + 'auth/login';
+const userUrl = root + 'api/user';
+const bookCategoryUrl = root + 'api/bookcategory';
+const bookUrl = root + 'api/books';
+export default {
+    authenticationUrl,
+    userUrl,
+    bookCategoryUrl,
+    bookUrl,
+    root
+};
